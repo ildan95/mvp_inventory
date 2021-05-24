d05039ed275c7fffabeb4abee56e83fbaf509098
ildan95 <ildan.kiamov@yandex.ru>
2021-05-25 00:17:49 +0300

    added allEmpty for generate item

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,20 +19,20 @@ namespace Game
             _icons = Resources.LoadAll<Sprite>("Images/Icons");
 
             var inventory = CreateInventory(10, 25);
-            var anotherInventory = CreateInventory(10, 25);
+            var anotherInventory = CreateInventory(10, 25, true);
             
             var model = new InventoryTransferModel(new[]{inventory, anotherInventory});
             
             _inventoryTransferPresenter = new InventoryTransferPresenter(_inventoryTransferView, model);
         }
 
-        private Inventory CreateInventory(int capacity, int maxCapacity)
+        private Inventory CreateInventory(int capacity, int maxCapacity, bool isAllEmpty = false)
         {
             var slots = new Slot[maxCapacity];
             for (int i = 0; i < maxCapacity; i++)
             {
                 var isClosed = i > capacity - 1;
-                var isEmpty = Random.Range(0f, 1f) > 0.5f;
+                var isEmpty = isAllEmpty || Random.Range(0f, 1f) > 0.5f;
                 
                 var item = isClosed || isEmpty  ? null : new Item(GetRandomIcon());
                 slots[i] = new Slot(item, i, isClosed);
